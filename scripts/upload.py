5d94da6799d6ccd5d8dd2266d0ac00c3dc290503
MalitsPlus <hua8102259@gmail.com>
2022-04-18 17:37:23 +0800

    feat: upload.py

---
+++ scripts/upload.py
@@ -0,0 +1,85 @@
+# Another script to upload Json files. 
+
+import sys
+import os
+import json
+import requests
+from time import sleep
+from pathlib import Path
+from rich.console import Console
+from requests.exceptions import SSLError
+from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
+
+API_ENDPOINT = "https://idoly-backend.ipri.workers.dev/manage/write"
+ADMIN_TOKEN = "ADMIN_TOKEN"
+DATA_PREFIX = "HSM_"
+MAX_RETRY_TIMES = 5
+MAX_WORK_THREAD = 20
+
+# Initializations
+console = Console()
+separators = (',', ':')
+headers = {
+    "Authorization": f"Bearer {ADMIN_TOKEN}"
+}
+
+def send_request(name: str, data: str, err_count: int = 0):
+    try:
+        r = requests.put(API_ENDPOINT, headers=headers, data=data)
+        if r.status_code == 200:
+            console.print(f"[bold green]>>> [Succeed][/bold green] Put '{name}' completed.")
+        else:
+            if err_count < MAX_RETRY_TIMES:
+                err_count += 1
+                console.print(
+                    f"[bold yellow]>>> [Warning][/bold yellow] Put '{name}' failed, status code: {r.status_code}. Retrying({err_count}/{MAX_RETRY_TIMES})...")
+                sleep(1)
+                send_request(name, data, err_count)
+            else:
+                console.print(
+                    f"[bold red]>>> [Error][/bold red] Maximum retries exceeded, stopping process.")
+                os._exit(-1)
+    except SSLError:
+        console.print(
+            f"[bold red]>>> [Error][/bold red] An SSLError has occured, please check your net work settings.")
+        os._exit(-1)
+    except:
+        console.print(
+            "[bold red]>>> [Error][/bold red] Unexpected error: ", sys.exc_info()[0])
+        os._exit(-1)
+
+def async_task(file: Path):
+    with file.open(mode="r", encoding="utf8") as fp:
+        name = file.name[0: -5]
+        origin = json.load(fp)
+        body = {
+            "key": DATA_PREFIX + name,
+            "value": origin
+        }
+        compacted = json.dumps(
+            body, ensure_ascii=False, separators=separators)
+        send_request(name, compacted)
+
+def main(): 
+    dir = Path("../data")
+    # Single thread test codes
+    # for file in dir.glob("*.json"):
+    #     with file.open(mode="r", encoding="utf8") as fp:
+    #         name = file.name[0: -5]
+    #         origin = json.load(fp)
+    #         body = {
+    #             "key": DATA_PREFIX + name,
+    #             "value": origin
+    #         }
+    #         compacted = json.dumps(
+    #             body, ensure_ascii=False, separators=separators)
+    #         send_request(name, compacted)
+
+    executor = ThreadPoolExecutor(max_workers=MAX_WORK_THREAD)
+    all_tasks = [executor.submit(async_task, file) for file in dir.glob("*.json")]
+    wait(all_tasks, return_when=ALL_COMPLETED)
+    console.print(
+        f"[bold green]>>> [Succeed][/bold green] Tasks all done.")
+
+if __name__ == "__main__":
+    main()
