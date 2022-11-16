1a6811f157b147645d1893eddfb640bc514dfc8a
Outvi V <git@outv.im>
2022-11-17 00:21:51 +0800

    chore(createErrStatus): suppress lint

--- denoland/backend/utils/createErrStatus.ts
+++ denoland/backend/utils/createErrStatus.ts
@@ -1,5 +1,6 @@
 import { FieldStatus } from './types.ts'
 
+// deno-lint-ignore no-explicit-any
 export default function createErrStatus(message: string, status = 400): any {
   return {
     [FieldStatus]: status,
