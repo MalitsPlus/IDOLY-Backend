20b6ee81bcf1ed51458f5ff405b35eb244c4328c
Outvi V <git@outv.im>
2023-10-09 20:45:50 +0800

    fix(Appearance/Hair): wtf

--- denoland/backend/routes/api/Appearance/Hair.ts
+++ denoland/backend/routes/api/Appearance/Hair.ts
@@ -2,14 +2,14 @@ import type { APIMapping } from 'hoshimi-types'
 import pick from 'lodash/pick'
 import { dbGet } from '@utils/dbGet.ts'
 import apiWrapper from '@utils/apiWrapper.ts'
-import filterRelease from '@utils/filterRelease.ts'
+import { filterReleaseTime } from '@utils/filterRelease.ts'
 import { parseMultiString } from '@utils/parse.ts'
 
 const responder: APIMapping['Appearance/Hair'] = async ({
   characterId: _characterId,
 }) => {
   const characterId = parseMultiString(_characterId)?.[0]
-  const hairs = await dbGet('Hair').then(filterRelease)
+  const hairs = await dbGet('Hair').then(filterReleaseTime)
   const ret = hairs
     .filter((x) => x.characterId === characterId)
     .sort((a, b) => a.order - b.order)
--- denoland/backend/utils/filterRelease.ts
+++ denoland/backend/utils/filterRelease.ts
@@ -5,3 +5,11 @@ export default function filterRelease<T extends { releaseDate: string }>(
     return Number(new Date(Number(x.releaseDate))) <= Number(new Date())
   })
 }
+
+export function filterReleaseTime<T extends { releaseTime: string }>(
+  items: T[]
+): T[] {
+  return items.filter((x) => {
+    return Number(new Date(Number(x.releaseTime))) <= Number(new Date())
+  })
+}
